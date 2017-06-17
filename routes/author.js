09f829de18bd30c06fa92c767214b725e05c0ace
TMineCola <wang.minecola@gmail.com>
2017-06-17 16:42:13 +0800

    add patch method for update author data

--- routes/author.js
+++ routes/author.js
@@ -25,7 +25,37 @@ router.get('/:username', function(req, res, next) {
       }
     }
   }
+});
 
+router.patch('/:username', function(req, res, next) {
+  let contents = fs.readFileSync("./data/author.json");
+  let jsonContent = JSON.parse(contents);
+  let target = req.params.username;
+  let postObj = req.body;
+  for(let i = 0; i < jsonContent.length; i++) {
+    if(jsonContent[i].username == target) {
+      jsonContent[i].name = postObj.name;
+      jsonContent[i].password = postObj.password;
+      jsonContent[i].gender = postObj.gender;
+      jsonContent[i].address = postObj.address;
+      let authorObj = {
+        "uesrname": jsonContent[i].username,
+        "name": jsonContent[i].name,
+        "gender": jsonContent[i].gender,
+        "address": jsonContent[i].address
+      };
+      fs.writeFile('./data/author.json', JSON.stringify(jsonContent), 'utf-8');
+      res.send(authorObj);
+      break;
+    } else {
+      if(i == jsonContent.length - 1) {
+        let errorObj = {
+          "message": "author not found"
+        };
+        res.send(errorObj);
+      }
+    }
+  }
 });
 
 module.exports = router;
