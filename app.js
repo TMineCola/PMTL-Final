eeca1782fc0062cc0a7f17897867337fafbc303e
TMineCola <wang.minecola@gmail.com>
2017-06-22 20:44:32 +0800

    test for cross domain with library cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var login = require('./routes/login');
@@ -24,6 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.options(cors());
+  
 app.use('/', index);
 app.use('/login', login);
 app.use('/author', author);
